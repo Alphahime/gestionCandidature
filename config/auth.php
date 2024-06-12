c50338caddc9fadc674b68e2e8ebed30d4a8be45
Oumyf <falladiaraoumy@gmail.com>
2024-06-12 16:41:06 +0000

    authentification du candidat en configurant auth.php dans config

--- config/auth.php
+++ config/auth.php
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'candidats',
         ],
     ],
 
@@ -60,9 +60,9 @@ return [
     */
 
     'providers' => [
-        'users' => [
+        'candidats' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => env('AUTH_MODEL', App\Models\Candidat::class),
         ],
 
         // 'users' => [
