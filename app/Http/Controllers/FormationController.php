d00a061f4ac0cc7c1d826ee056903edba53a7533
fatima-harun <faabintu@gmail.com>
2024-06-13 11:21:28 +0000

    stylisation de la page de detail formation

--- app/Http/Controllers/FormationController.php
+++ app/Http/Controllers/FormationController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class FormationController extends Controller
 {
-    //
+    public function detail(){
+        return view('formations.indexdetail');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormationController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/detailFormation',[FormationController::class, 'detail']);
